75faf027df77451b3fe735b28e6c7afe18e27c3f
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-18 23:13:45 +0900

    config

--- config.js
+++ config.js
@@ -1,2 +1,14 @@
-exports.namespace = 'cde'
-exports.cde_image = 'moreillon/cde:20220916-1801'
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const {
+    KUBECONFIG_PATH = '/kubeconfig',
+    NAMESPACE = 'cde',
+    CDE_IMAGE = 'moreillon/cde:20220916-1801'
+} = process.env
+
+
+exports.kubeconfig_path = KUBECONFIG_PATH
+exports.namespace = NAMESPACE
+exports.cde_image = CDE_IMAGE
--- k8s.js
+++ k8s.js
@@ -1,7 +1,7 @@
 const k8s = require('@kubernetes/client-node')
-
+const { kubeconfig_path } = require('./config')
 const kc = new k8s.KubeConfig()
-kc.loadFromFile('./kubeconfig')
+kc.loadFromFile(kubeconfig_path)
 
 const api = kc.makeApiClient(k8s.CoreV1Api)
 
